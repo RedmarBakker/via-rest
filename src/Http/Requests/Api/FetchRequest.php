7f483a8fc7a4af8c79b87daa30b8bf49889389a6
Jesse Gall <jesse-gall@hotmail.com>
2019-06-24 21:26:34 +0200

    Added base FetchRequest

---
+++ src/Http/Requests/Api/FetchRequest.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace ViaRest\Http\Requests\Api;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class FetchRequest extends FormRequest implements CrudRequestInterface
+{
+
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        if (env('APP_DEBUG') == true) {
+            return true;
+        }
+
+        return auth()->check();
+    }
+
+    /**
+     * Pass the failed authorization, we handle this in the AbstractRestController it self.
+     * */
+    public function failedAuthorization()
+    {
+        return;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'order_identifier' => 'max:255',
+            'order_direction'  => 'max:4',
+            'limit'            => 'integer',
+        ];
+    }
+
+}
